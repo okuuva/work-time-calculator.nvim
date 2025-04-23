e63bf3067c240cf0686bfc137b5fe0531a28f0dc
Oula Kuuva <oula.kuuva@iki.fi>
2025-04-23 08:52:13 +0300

    feat: go to insert mode at the last line
    This makes markdown-table-mode to reformat the table if it's installed
    and enabled. Need to see if it supports in-place reformatting. If not, I
    should add that.

--- lua/work-time-calculator/init.lua
+++ lua/work-time-calculator/init.lua
@@ -9,15 +9,14 @@ local M = {}
 M.config = config
 
 ---@param args wtc.Config?
--- you can define your setup function here. Usually configurations can be merged, accepting outside params and
--- you can also put some validation here for those.
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
 M.calculate_time = function()
   time_table.generate_hours_table(M.config)
-  vim.cmd("edit! " .. M.config.output_file)
+  vim.cmd("edit! " .. M.config.output_file) -- open the output file
+  vim.cmd("normal Gi") -- go to insert mode on the last line so markdown-table-mode can reformat the table
 end
 
 return M
