e879472a9421197e66dcc656ca22a253fcb8036b
Oula Kuuva <oula.kuuva@iki.fi>
2025-06-12 20:53:27 +0300

    fix: default config

--- lua/work-time-calculator/config.lua
+++ lua/work-time-calculator/config.lua
@@ -1,12 +1,12 @@
 ---@class wtc.Config
----@field daily_notes_dir string Path to the daily notes directory
----@field output_file string Path to the output file
+---@field daily_notes_dir string? Path to the daily notes directory
+---@field output_file string? Path to the output file
 ---@field workday_length string Expected workday length in HH:MM format
 local default_config = {
   -- FIXME: fetch these paths with obsidian.nvim
-  daily_notes_dir = vim.fn.expand("~/Notes/notes/dailies"),
-  output_file = vim.fn.expand("~/Notes/notes/1740225022-hours.md"),
-  workday_length = "06:00",
+  daily_notes_dir = nil,
+  output_file = nil,
+  workday_length = "07:30",
 }
 
 return default_config
